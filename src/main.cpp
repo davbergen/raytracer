f679488276aecefd062b95084bcc1162add7cd6f
David Bergendorff <david.bergendorff@gmail.com>
2024-12-09 19:00:41 +0100

    Add glm to dependencies and build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include "glm/glm.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "Hello, World! The sin of 90 degrees is about " << glm::sin(glm::radians(90.f)) << std::endl;
     return 0;
 }
